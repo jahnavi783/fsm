fe6e1c9738a3c92795d8cbb50d35c8e14fad3a35
jahnavis <jahnavi.singampalli@global-csg.com>
2025-12-05 11:18:28 +0530

    feat: Add userId to WorkOrderEntity and implement local user storage for offline filtering

--- lib/features/work_orders/data/services/local_user_store.dart
+++ lib/features/work_orders/data/services/local_user_store.dart
@@ -1,29 +1,68 @@
+// // import 'package:hive_ce/hive.dart';
+// //
+// // class LocalUserStore {
+// //   static const _boxName = 'user_box';
+// //   static const _keyUserId = 'current_user_id';
+// //
+// //   Future<void> saveUserId(String userId) async {
+// //     final box = await Hive.openBox(_boxName);
+// //     await box.put(_keyUserId, userId);
+// //   }
+// //
+// //   Future<String?> getUserId() async {
+// //     final box = await Hive.openBox(_boxName);
+// //     return box.get(_keyUserId);
+// //   }
+// //
+// //   Future<void> clearUserId() async {
+// //     final box = await Hive.openBox(_boxName);
+// //     await box.delete(_keyUserId);
+// //   }
+// // }
+// // import 'package:hive_ce/hive.dart';
+// //
+// // class LocalUserStore {
+// //   static const _boxName = 'user_box';
+// //   static const _keyUserId = 'current_user_id';
+// //
+// //   Future<void> saveUserId(String userId) async {
+// //     final box = await Hive.openBox(_boxName);
+// //     await box.put(_keyUserId, userId);
+// //   }
+// //
+// //   Future<String?> getUserId() async {
+// //     final box = await Hive.openBox(_boxName);
+// //     return box.get(_keyUserId);
+// //   }
+// //
+// //   Future<void> clearUserId() async {
+// //     final box = await Hive.openBox(_boxName);
+// //     await box.delete(_keyUserId);
+// //   }
+// // }
 // import 'package:hive_ce/hive.dart';
+// import 'package:injectable/injectable.dart';
 //
+// @lazySingleton
 // class LocalUserStore {
 //   static const _boxName = 'user_box';
 //   static const _keyUserId = 'current_user_id';
+//   static const _keyAccessToken = 'access_token';
 //
-//   Future<void> saveUserId(String userId) async {
+//   Future<void> saveAccessToken(String token) async {
 //     final box = await Hive.openBox(_boxName);
-//     await box.put(_keyUserId, userId);
+//     await box.put(_keyAccessToken, token);
 //   }
 //
-//   Future<String?> getUserId() async {
+//   Future<String?> getAccessToken() async {
 //     final box = await Hive.openBox(_boxName);
-//     return box.get(_keyUserId);
+//     return box.get(_keyAccessToken);
 //   }
 //
-//   Future<void> clearUserId() async {
+//   Future<void> clearAccessToken() async {
 //     final box = await Hive.openBox(_boxName);
-//     await box.delete(_keyUserId);
+//     await box.delete(_keyAccessToken);
 //   }
-// }
-// import 'package:hive_ce/hive.dart';
-//
-// class LocalUserStore {
-//   static const _boxName = 'user_box';
-//   static const _keyUserId = 'current_user_id';
 //
 //   Future<void> saveUserId(String userId) async {
 //     final box = await Hive.openBox(_boxName);
@@ -48,7 +87,9 @@ class LocalUserStore {
   static const _boxName = 'user_box';
   static const _keyUserId = 'current_user_id';
   static const _keyAccessToken = 'access_token';
+  static const _keyRefreshToken = 'refresh_token';
 
+  // ------------------- ACCESS TOKEN -------------------
   Future<void> saveAccessToken(String token) async {
     final box = await Hive.openBox(_boxName);
     await box.put(_keyAccessToken, token);
@@ -64,6 +105,33 @@ class LocalUserStore {
     await box.delete(_keyAccessToken);
   }
 
+  // ------------------- REFRESH TOKEN -------------------
+  Future<void> saveRefreshToken(String token) async {
+    final box = await Hive.openBox(_boxName);
+    await box.put(_keyRefreshToken, token);
+  }
+
+  Future<String?> getRefreshToken() async {
+    final box = await Hive.openBox(_boxName);
+    return box.get(_keyRefreshToken);
+  }
+
+  Future<void> clearRefreshToken() async {
+    final box = await Hive.openBox(_boxName);
+    await box.delete(_keyRefreshToken);
+  }
+
+  // Save both tokens together (used at login)
+  Future<void> saveTokens({
+    required String accessToken,
+    required String refreshToken,
+  }) async {
+    final box = await Hive.openBox(_boxName);
+    await box.put(_keyAccessToken, accessToken);
+    await box.put(_keyRefreshToken, refreshToken);
+  }
+
+  // ------------------- USER -------------------
   Future<void> saveUserId(String userId) async {
     final box = await Hive.openBox(_boxName);
     await box.put(_keyUserId, userId);
